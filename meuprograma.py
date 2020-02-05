7b783aa3ab9029ce0dbb9941274aeae33e7d0b6a
Daniela <danifavero@usp.br>
2020-02-05 15:12:24 -0300

    remove lixo

--- meuprograma.py
+++
@@ -1,8 +0,0 @@
-# 2.9 Primeiro programa
-print('Minha primeira aplicação Python!')
-
-# 2.10 Exemplos
-print('Um')
-print('Dois')
-
-print('Um\n Dois')
--- programa.py
+++
@@ -1,8 +0,0 @@
-# 3.4 Instruções
-mensagem = "oi, python"
-numero = 5
-pi = 3.14
-
-print(mensagem)
-print(numero)
-print(pi)
