3dd0ac9a6667d160e11543f2440cde1b724195bb
Daniela <danifavero@usp.br>
2020-02-13 11:54:27 -0300

    aula 4

--- oo/caixaeletronico.py
+++ oo/caixaeletronico.py
@@ -3,4 +3,28 @@
 from conta_excecoes import *
 
 class CaixaEletronico:
-    pass
+
+    def saque(self, conta, valor):
+        try:
+            conta.saca(valor)
+            print(f'Saque de {valor} realizado com sucesso')
+        except ValueError:
+            print('O valor a ser sacado deve ser um número positivo.')
+        except SaldoInsuficienteError:
+            print('Você não possui saldo suficiente para concluir esta operação.')
+    
+    def deposito(self, conta, valor):
+        try:
+            conta.deposita(valor)
+            print(f'Depósito de {valor} realizado com sucesso.')
+        except ValueError:
+            print('O valor a ser depositado deve ser um número positivo.')
+
+if __name__ == "__main__":
+    caixa = CaixaEletronico()
+    cc = ContaCorrente('123-4', 'João', 1000.0)
+    cp = ContaPoupanca('123-5', 'José', 800.0)
+    
+    caixa.saque(cc, 5000)
+    caixa.saque(cp, -1000)
+    caixa.deposito(cc, -10)
